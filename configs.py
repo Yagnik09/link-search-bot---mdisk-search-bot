a4f162231afff316b84795cfdaa63821a6aeb580
Yagnik09 <96000088+Yagnik09@users.noreply.github.com>
2022-12-12 01:10:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,6 +11,7 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DATABASE_URL = os.environ.get("DATABASE_URL")
+    FORCE_SUB = os.getenv("FORCE_SUB", "False")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Link Search Bot.
     
