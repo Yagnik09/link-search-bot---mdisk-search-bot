993734e437bbc2cd3bb7d3b540f9bd1e0e4c579e
Yagnik09 <96000088+Yagnik09@users.noreply.github.com>
2022-12-12 11:03:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
+git clone https://github.com/Yagnik09/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
 cd /Mdisk-Search-Bot
 pip3 install -r requirements.txt
-echo "Starting Bot....GreyMatter_Bots"
+echo "Starting Bot....ATM_Film_HD"
 python3 main.py
